0556ac177803dc1075e1ad6355bcd82d97ad4bf9
Mehedii-07 <mehedihasan2272002@gmail.com>
2026-08-12 23:29:19 +0600

    Implement HTTP interceptor to attach JWT tokens

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,12 +1,13 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { authInterceptor } from './interceptors/auth'; // Import your interceptor
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideRouter(routes),
+    // Add withInterceptors to your HTTP client provider
+    provideHttpClient(withInterceptors([authInterceptor])) 
   ]
 };
